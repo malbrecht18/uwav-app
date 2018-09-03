2ed6db51506c1afc82dcf0ca73139173e222e94d
clementfarolet <clement.farolet@gmail.com>
2018-09-03 11:31:07 +0200

    style tabNavigator

--- app/navigation/MainTabNavigator.js
+++ app/navigation/MainTabNavigator.js
@@ -5,6 +5,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import TestNavigator from '../screens/TestNavigator';
+import TestNavigator2 from '../screens/TestNavigator2';
 import ScreenTest from '../screens/ScreenTest';
 
 import { MaterialIcons } from '@expo/vector-icons';
@@ -14,12 +15,12 @@ import  ButtonConnection  from '../components/ButtonConnection/ButtonConnection'
 
 class TabIcon extends React.Component {
   render() {
-    let baseSize = this.props.size || 26;
+    let baseSize = this.props.size || 32;
     return (
       <MaterialIcons
         name={this.props.name}
         size={Platform.OS === 'android' ? baseSize - 2 : baseSize}
-        color={this.props.focused ? 'black' : 'black'}
+        color={this.props.focused ? 'white' : 'black'}
       />
     );
   }
@@ -31,6 +32,7 @@ const MainTabNavigator = createTabNavigator({
 
   NavigatorTest: {screen: TestNavigator},
   TestScreen: {screen: ScreenTest},
+  NavigatorTest2: {screen: TestNavigator2},
 
   },
   {
@@ -43,15 +45,42 @@ const MainTabNavigator = createTabNavigator({
 
       return {
         header: null,
-        tabBarLabel,
+        tabBarLabel,   
         tabBarIcon: ({ focused }) => {
           const { routeName } = navigation.state;
           if (routeName == 'NavigatorTest'){
-            return <TabIcon name="group-work" focused={focused} />;
+            return <TabIcon name="search" focused={focused} />;
           }
-        },
-      };
+          if (routeName == 'TestScreen'){
+            return <TabIcon name="playlist-play" focused={focused} />;
+          }
+          if (routeName == 'NavigatorTest2'){
+            return <TabIcon name="history" focused={focused} />;
+          }          
+        },      
+      };      
     },
+    tabBarOptions: {
+      tinColor: 'black',
+      activeTintColor: 'white',
+      inactiveTintColor: 'black',
+      showIcon: true,
+      showLabel: true,
+      lazyLoad: true,
+      upperCaseLabel: false,
+      indicatorStyle: {
+        backgroundColor: 'transparent'
+      },
+      style: {
+        backgroundColor: 'rgba(66, 175, 112, 0.1)',
+        borderTopWidth: 3,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 15,
+        height: 75,
+      }
+    }
   });
 
   export default MainTabNavigator;
---
+++ app/screens/TestNavigator2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { LinearGradient, Font } from 'expo';
+import { createStackNavigator } from 'react-navigation';
+import { createMaterialBottomTabNavigator, createBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
+import { ButtonTest } from '../components/ButtonTest/ButtonTest';
+import  Title  from '../components/Title/Title';
+import  ButtonConnection  from '../components/ButtonConnection/ButtonConnection';
+
+export default class TestNavigator extends React.Component {
+
+  render() {    
+    return (
+      <View style={{ flex: 1 , flexDirection: 'row'}}>
+        <View style={{ backgroundColor: '#5de2b4', flexDirection: 'row' }} />        
+        <LinearGradient colors={['rgba(0,255,255,0.7)', '#42af70']} style={styles.container}>      
+            <Title/>
+            <Text style={styles.textStyle}>petit con</Text>
+        </LinearGradient>                 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',    
+    height: "100%",
+    width: "100%",    
+  },
+  textStyle: {
+    fontSize: 64,
+    color: 'white',
+  },
+});
